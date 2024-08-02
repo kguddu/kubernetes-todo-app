803215776ebc559d4b291bd5846d33f4e7aeb0e9
guddu <kguddu.1011@gmail.com>
2024-08-02 12:19:55 +0530

    file commite

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/todos', {
+mongoose.connect('mongodb:// mongodb-cluser-ip-serv/todos', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
